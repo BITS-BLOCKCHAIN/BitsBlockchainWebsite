3415da76b9182c6b1a26a369e58d087974e9301f
Viren <mundhalviren@gmail.com>
2024-04-27 04:54:07 +0530

    Added New Fonts and Images

--- BitsBlockChainWebsite/tailwind.config.js
+++ BitsBlockChainWebsite/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'kola': ['Kola', 'sans-serif'],
+    },
+    },
   },
   plugins: [],
 }
