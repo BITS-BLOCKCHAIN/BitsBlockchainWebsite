862d2da07e992c295bce91fff2ae5bc9fb027d24
Viren <mundhalviren@gmail.com>
2024-04-27 04:58:08 +0530

    Added New Typing Text Section
    Typing animation Section

--- BitsBlockChainWebsite/src/App.jsx
+++ BitsBlockChainWebsite/src/App.jsx
@@ -4,12 +4,21 @@ import TeamComponent from './Components/TeamComponent'
 import Header from './Components/Header';
 import AboutUs from './about';
 import Info from './info';
+import TypingText from "./TypingText";
+import "./Typing.css";
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
     <Header/>
+    <TypingText
+        dynamicTexts={[
+          "BITS BlockChain Hub",
+          "The World of Web3 ",
+          "The Crypto Age",
+        ]}
+      />
     <div id="about-us">
                 <AboutUs />
             </div>
---
+++ BitsBlockChainWebsite/src/TypingText.jsx
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import "./Typing.css";
+
+const TypingText = ({ dynamicTexts }) => {
+    const staticText = "Welcome to";
+
+    const [staticTextTyped, setStaticTextTyped] = useState("");
+    const [isStaticTextComplete, setIsStaticTextComplete] = useState(false);
+    const [dynamicTextIndex, setDynamicTextIndex] = useState(0);
+    const [dynamicTextTyped, setDynamicTextTyped] = useState("");
+
+    useEffect(() => {
+        if (!isStaticTextComplete) {
+            const interval = setInterval(() => {
+                if (staticTextTyped.length < staticText.length) {
+                    setStaticTextTyped(staticText.substring(0, staticTextTyped.length + 1));
+                } else {
+                    clearInterval(interval);
+                    setIsStaticTextComplete(true);
+                }
+            }, 100);
+            return () => clearInterval(interval);
+        }
+    }, [staticTextTyped, isStaticTextComplete, staticText]);
+
+    useEffect(() => {
+        if (isStaticTextComplete) {
+            const interval = setInterval(() => {
+                const currentDynamicText = dynamicTexts[dynamicTextIndex];
+                if (dynamicTextTyped.length < currentDynamicText.length) {
+                    setDynamicTextTyped((prevText) =>
+                        currentDynamicText.substring(0, prevText.length + 1)
+                    );
+                } else {
+                    clearInterval(interval);
+                    setTimeout(() => {
+                        setDynamicTextIndex((dynamicTextIndex + 1) % dynamicTexts.length);
+                        setDynamicTextTyped("");
+                    }, 1000);
+                }
+            }, 70);
+            return () => clearInterval(interval);
+        }
+    }, [isStaticTextComplete, dynamicTextIndex, dynamicTextTyped, dynamicTexts]);
+
+    return (
+        <div className="parent-container mt-20 pb-20 pl-10 pr-10 pt-20 shadow-xl border-4 rounded-xl"
+        style={{ borderColor: 'hsl(65, 100%, 89%)' }}>
+            <div className="text-column">
+                <div className="static-text ">
+                    {staticTextTyped}
+                </div>
+                {isStaticTextComplete && (
+                    <div className="typing-text">
+                        {dynamicTextTyped}
+                    </div>
+                )}
+            </div>
+            <div className="image-column">
+                <img src="/images/image2.png" alt="Random Image" className="typing-image" />
+            </div>
+        </div>
+    );
+};
+
+export default TypingText;
+
