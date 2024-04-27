4433bdf8625553e57d7133c93b5c4a6f9dd40c8e
Viren <mundhalviren@gmail.com>
2024-04-27 17:01:19 +0530

    Added Animation in the typing text section

--- BitsBlockChainWebsite/src/TypingText.jsx
+++ BitsBlockChainWebsite/src/TypingText.jsx
@@ -46,6 +46,9 @@ const TypingText = ({ dynamicTexts }) => {
     return (
         <div className="parent-container mt-20 pb-20 pl-10 pr-10 pt-20 shadow-xl border-4 rounded-xl"
         style={{ borderColor: 'hsl(65, 100%, 89%)' }}>
+         <video className="background-video" autoPlay loop muted>
+                <source src='/images/backvideo.mp4' type="video/mp4" />
+            </video>
             <div className="text-column">
                 <div className="static-text ">
                     {staticTextTyped}
