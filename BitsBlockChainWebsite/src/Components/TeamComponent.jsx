7aaebd2117b0c3f29dfbda83ebb45f6a40cd7880
Devansh <f20220457@pilani.bits-pilani.ac.in>
2024-04-28 14:02:47 +0530

    made changes in the file structure

--- BitsBlockChainWebsite/src/Components/TeamComponent.jsx
+++ BitsBlockChainWebsite/src/Components/TeamComponent.jsx
@@ -3,7 +3,7 @@ import CardComponent from "./TeamMemberCard";
 
 const TeamComponent=()=>{
     return (
-        <div className="grid grid-cols-3 align-center px-20 mt-20 gap-8 mb-10">
+        <div className="grid md:grid-cols-3 align-center sm:px-20 mt-20 gap-8 mb-10 ">
             {TeamMembers.map((member, index) => (
                 <div key={index}  className="rounded-xl grid grid-cols-5 border-black border-2 px-5 py-5 h-6/6">
                     <CardComponent {...member} />
