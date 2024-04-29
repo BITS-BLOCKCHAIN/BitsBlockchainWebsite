3437bf136ee91c5bc89b9e861c5c01e8e5febad7
Viren <mundhalviren@gmail.com>
2024-04-29 19:34:41 +0530

    Improved responsiveness in Typing Text Section

--- BitsBlockChainWebsite/src/Data.js
+++ BitsBlockChainWebsite/src/Data.js
@@ -31,7 +31,7 @@ const TeamMembers = [
     name: "Viren Singh",
     LinkedIn: "",
     image: "../../images/Viren.jpg",
-    Github: "",
+    Github: "https://github.com/Anonymous2512",
     position: "Full Stack Developer",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
