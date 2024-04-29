4b70ea7c6eb99e46c54a3b2d1d0f679dae9f0ca9
Pranay <pranayjalan.work@gmail.com>
2024-04-29 12:50:51 +0530

    updated members' data

--- BitsBlockChainWebsite/src/Data.js
+++ BitsBlockChainWebsite/src/Data.js
@@ -1,7 +1,7 @@
 const TeamMembers = [
   {
     name: "Dhruv Varshney",
-    image: "../../images/DevanshGoel.jpeg",
+    image: "../../images/dhruv varshney.png",
     LinkedIn: "",
     Github: "",
     position: "President",
@@ -19,9 +19,10 @@ const TeamMembers = [
   },
   {
     name: "Pranay Jalan",
-    image: "../../images/DevanshGoel.jpeg",
-    LinkedIn: "",
-    Github: "",
+    image: "../../images/pranay.jpg",
+    LinkedIn:
+      "https://www.linkedin.com/in/pranay-jalan254?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    Github: "https://github.com/pranayjalan254",
     position: " Frontend Developer",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
@@ -32,6 +33,101 @@ const TeamMembers = [
     image: "../../images/Viren.jpg",
     Github: "",
     position: "Full Stack Developer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Rudraksh",
+    LinkedIn: "linkedin.com/in/rudraksh-singhal-31a197253/",
+    image: "../../images/rudraksh.jpg",
+    Github: "github.com/rabbity-00",
+    position: "Head, Blockchain Week",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Sidak Malhotra",
+    LinkedIn: "https://www.linkedin.com/in/sidak-malhotra-0743b4183/",
+    image: "../../images/sidak.jpg",
+    Github: "https://github.com/Psyduck-m",
+    position: "Outreach Lead",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Anmol Agrawal",
+    LinkedIn:
+      "https://www.linkedin.com/in/anmol-agrawal-2a767a229?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    image: "../../images/anmol.jpg",
+    Github: "https://github.com/anmolagr39",
+    position: "Partnership Team",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Maneet Kaur",
+    LinkedIn: "https://www.linkedin.com/in/maneet-kaur-2140ba217",
+    image: "../../images/maneet.jpg",
+    Github: "https://github.com/maneet-kaur-13",
+    position: "Community Manager",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Atharv",
+    LinkedIn:
+      "https://www.linkedin.com/in/atharv-kulshreshtha-67922b283?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    image: "../../images/atharv.jpg",
+    Github: "https://github.com/atharvk1804",
+    position: "Community Manager",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "V Dhananjay",
+    LinkedIn:
+      "https://www.linkedin.com/in/v-dhananjaya-raju-78ba8926b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    image: "../../images/dhananjay.jpg",
+    Github: "",
+    position: "Community Manager",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Shubham",
+    LinkedIn:
+      "https://www.linkedin.com/in/shubham-mishra-61383b228?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    image: "../../images/shubham mishra.jpg",
+    Github: "https://github.com/ShubhamX90",
+    position: "Development Core",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+
+  {
+    name: "Arnav",
+    LinkedIn: "",
+    image: "../../images/arnav.jpg",
+    Github: "",
+    position: "Social Media Manager",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Satyam",
+    LinkedIn: "www.linkedin.com/in/satyam-811543275",
+    image: "../../images/satyam.jpg",
+    Github: "https://github.com/Satyamk33",
+    position: "Developer",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
+  },
+  {
+    name: "Divyansh Jha",
+    LinkedIn: "https://www.linkedin.com/in/divyaanshjha",
+    image: "../../images/divyansh.jpeg",
+    Github: "https://github.com/divyaanshjha",
+    position: "Member",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod justo ut mi ultricies, ut accumsan libero tristique. Vivamus non risus auctor, vestibulum ipsum ac, convallis nisi. Maecenas vitae ullamcorper ex.",
   },
