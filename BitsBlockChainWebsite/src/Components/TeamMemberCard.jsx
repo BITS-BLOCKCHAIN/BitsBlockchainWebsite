9af83ab8382cbf0ddc8031f7d8b0e2689f8a69c9
Viren <mundhalviren@gmail.com>
2024-04-29 18:54:43 +0530

    Fixed image skewed issue in our team section

--- BitsBlockChainWebsite/src/Components/TeamMemberCard.jsx
+++ BitsBlockChainWebsite/src/Components/TeamMemberCard.jsx
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa"
 const cardComponent=(props)=>{
     return <>
    
-    <img className="size-65 xs:size-75 md:size-70 xl:size-60 xs:aspect-square ml-auto mr-auto" src={props.image}></img>
+    <img className="size-65 xs:size-75 md:size-70 xl:size-60 xs:aspect-square ml-auto mr-auto object-cover rounded-xl" src={props.image}></img>
     <div className="flex flex-col justify-top align-center col-span-2 pt-2 leading-5">
            <h2 className="text-2xl font-semibold self-center">{props.name}</h2>
            <h4 className="text-md self-center">{props.position}</h4>
