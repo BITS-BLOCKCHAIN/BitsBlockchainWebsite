8f6d2049d04940d2cd13647288ef637f76ef17bf
Pranay <pranayjalan.work@gmail.com>
2024-04-30 21:35:52 +0530

    updates

--- BitsBlockChainWebsite/src/App.jsx
+++ BitsBlockChainWebsite/src/App.jsx
@@ -1,40 +1,40 @@
-import { useState } from 'react'
-import Footer from './Components/Footer'
-import TeamComponent from './Components/TeamComponent'
-import Header from './Components/Header';
-import AboutUs from './Components/about';
-import Info from './Components/info';
+import { useState } from "react";
+import Footer from "./Components/Footer";
+import TeamComponent from "./Components/TeamComponent";
+import Header from "./Components/Header";
+import AboutUs from "./Components/about";
+import Info from "./Components/info";
 import TypingText from "./Components/TypingText";
 import "./Css/Typing.css";
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <Header/>
-    <TypingText
+      <Header />
+      <TypingText
         dynamicTexts={[
           "BITS BlockChain Hub",
           "The World of Web3 ",
           "The Crypto Age",
         ]}
       />
-    <div id="about-us">
-                <AboutUs />
-            </div>
-            <div id="info">
-                <Info/>
-            </div>
-            <div id="our-team">
-               <h1 className="text-3xl font-kola text-center">Meet Our Team</h1>
-                <TeamComponent />
-            </div>
-            <div id="contact-us">
-            <Footer />
-            </div>
-        </>
-   
-  )
+      <div id="about-us">
+        <AboutUs />
+      </div>
+      <div id="info">
+        <Info />
+      </div>
+      <div id="our-team">
+        <h1 className="text-3xl font-kola text-center">Meet Our Team</h1>
+        <TeamComponent />
+      </div>
+      <div id="contact-us">
+        <Footer />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
