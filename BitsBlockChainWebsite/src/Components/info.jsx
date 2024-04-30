523ca4bb666217cb281bce6d1b9db6823b6a7583
Pranay <pranayjalan.work@gmail.com>
2024-04-30 19:13:53 +0530

    updated previous events

--- BitsBlockChainWebsite/src/Components/info.jsx
+++ BitsBlockChainWebsite/src/Components/info.jsx
@@ -1,55 +1,70 @@
-import React from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
-import '../Css/info.css';
+import React from "react";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "../Css/info.css";
 
 const Info = () => {
-    const carouselSettings = {
-        responsive: {
-            all: {
-                breakpoint: { max: 4000, min: 0 },
-                items: 1,
-            },
-        },
-        infinite: true,
-        autoPlay: true,
-        autoPlaySpeed: 3000,
-        keyBoardControl: true,
-        transitionDuration: 500,
-    };
+  const carouselSettings = {
+    responsive: {
+      all: {
+        breakpoint: { max: 4000, min: 0 },
+        items: 1,
+      },
+    },
+    infinite: true,
+    autoPlay: true,
+    autoPlaySpeed: 3000,
+    keyBoardControl: true,
+    transitionDuration: 500,
+  };
 
-    return (
-        <div className="info-container">
-            <div className="info-column">
-                <h2>Our Projects</h2>
-                <Carousel {...carouselSettings}>
-                <div className="carousel-item">
-                        <img src="https://picsum.photos/803/400" alt="Random Event 1" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://picsum.photos/804/400" alt="Random Event 2" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://picsum.photos/805/400" alt="Random Event 3" />
-                    </div>  
-                </Carousel>
-            </div>
-                <div className="info-column">
-                <h2>Upcoming Events</h2>
-                <Carousel {...carouselSettings}>
-                <div className="carousel-item">
-                        <img src="https://picsum.photos/803/400" alt="Random Event 1" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://picsum.photos/804/400" alt="Random Event 2" />
-                    </div>
-                    <div className="carousel-item">
-                        <img src="https://picsum.photos/805/400" alt="Random Event 3" />
-                    </div> 
-                </Carousel>
-            </div>
-        </div>
-    );
+  return (
+    <div className="info-container">
+      <div className="info-column">
+        <h2>Previous Events</h2>
+        <Carousel {...carouselSettings}>
+          <div className="carousel-item">
+            <img
+              src="https://pbs.twimg.com/media/FVDCURPVIAAk--B?format=jpg&name=large"
+              alt="Panel discussion"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://pbs.twimg.com/media/FUPBAzKaQAA-qXI?format=jpg&name=large"
+              alt="Hackathon"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://pbs.twimg.com/media/FVhHeeoVIAAH0iK?format=jpg&name=large"
+              alt="Random Event 3"
+            />
+          </div>
+          <div className="carousel-item">
+            <img
+              src="https://pbs.twimg.com/media/FUT6AcTUcAA1hhn?format=jpg&name=large"
+              alt="Random Event 3"
+            />
+          </div>
+        </Carousel>
+      </div>
+      <div className="info-column">
+        <h2>Upcoming Events</h2>
+        <Carousel {...carouselSettings}>
+          <div className="carousel-item">
+            <img src="https://picsum.photos/803/400" alt="Random Event 1" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://picsum.photos/804/400" alt="Random Event 2" />
+          </div>
+          <div className="carousel-item">
+            <img src="https://picsum.photos/805/400" alt="Random Event 3" />
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
